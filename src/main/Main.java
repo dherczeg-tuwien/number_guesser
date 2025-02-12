eec8288fb0fa367bbafb4f0f31bcc5bcf1c0b9eb
Daniel Herczeg <daniel.herczeg@tuwien.ac.at>
2025-02-12 13:30:25 +0100

    Refactoring: extract mathod

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,11 +8,15 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     static Random rand = new Random();
 
-    public static void main(String[] args) {
-        int number_of_computer = rand.nextInt(101);
-
+    static int read_player_number () {
         System.out.print("Enter your number: ");
         int player_quess = scanner.nextInt();
+        return player_quess;
+    }
+
+    public static void main(String[] args) {
+        int number_of_computer = rand.nextInt(101);
+        int player_quess = read_player_number ();
 
         while (player_quess != number_of_computer) {
             System.out.println ("false number");
@@ -23,8 +27,7 @@ public class Main {
                 System.out.println("number should be greater");
             }
 
-            System.out.print ("Enter your number: ");
-            player_quess = scanner.nextInt();
+            player_quess = read_player_number ();
         }
 
         if (player_quess == number_of_computer) {
