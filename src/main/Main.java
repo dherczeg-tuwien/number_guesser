b98b53db09ed888c8dfd70bcb22223f30b541bbd
Daniel Herczeg <daniel.herczeg@tuwien.ac.at>
2025-02-12 13:17:26 +0100

    Refactoring: Fixed Layout

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,43 +3,32 @@ package main;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     static Scanner scanner = new Scanner(System.in);
     static Random rand = new Random();
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
         int number_of_computer = rand.nextInt(101);
 
         System.out.print("Enter your number: ");
         int player_quess = scanner.nextInt();
 
-
         while (player_quess != number_of_computer) {
-
-
             System.out.println ("false number");
 
             if (player_quess > number_of_computer) {
                 System.out.println("number should be smaller");
-            }
-
-            else {
+            } else {
                 System.out.println("number should be greater");
-
             }
+
             System.out.print ("Enter your number: ");
-
             player_quess = scanner.nextInt();
+        }
 
-            }
         if (player_quess == number_of_computer) {
             System.out.print ("You win");
         }
-
     }
-
 }
