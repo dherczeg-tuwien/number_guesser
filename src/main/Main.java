02c816233f2a90fc63ac7bd65b5a360ade714543
Daniel Herczeg <daniel.herczeg@tuwien.ac.at>
2025-02-13 11:36:49 +0100

    added use input loop

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,18 +11,42 @@ public class Main {
     static Random rand = new Random();
     static int numbers_of_quesses = 1;
 
-    static int read_player_number () {
-        System.out.print("Enter your number between 0 and " + (COMPUTER_GUESS_RANGE - 1) + ": ");
-        int player_quess = -1;
+    /**
+     * bestimmt ob der gebebenen Zeichen in einer ganzen Zahl umgewandelt werden könnnen.
+     * Wenn nicht, wird ein Fehler ausgegeben und wird -1 zurückgegeben.
+     * Wenn die Zahl gültig war, wird bestimmt, ob die Zahl positiv ist und kleiner als
+     * COMPUTER_QUESS_RANGE ist.
+     * Wenn die Zahl in Definitionsbereich ist, wird sie zurückgegeben, sonst wird ein Fehler
+     * angezeigt und -1 zurückgegeben.
+     * @param player_quess ist die Zahl, die ungewandelt werden sollen.
+     * @return ungewandelte Zahl güötig sonst -1
+     */
+    private static int convertPlayerInputToNumber(String player_quess) {
+        int convertedQuess;
         try {
-            player_quess = scanner.nextInt();
-            if (player_quess > COMPUTER_GUESS_RANGE || player_quess < 0) {
-                System.out.println("You have entered an invalid number!");
-            }
+            convertedQuess = Integer.parseInt(player_quess);
         } catch (Exception e) {
             System.out.println("it should be a number");
-            scanner.next();
+            return -1;
         }
+        if (convertedQuess > COMPUTER_GUESS_RANGE || convertedQuess < 0) {
+            System.out.println("You have entered an invalid number!");
+            return -1;
+        }
+        return convertedQuess;
+    }
+
+    /**
+     * die Funktion liest solange Eingaben ein, bis der Spieler eine Zahl zwishcen 0 und COMPUTER_GUESS_RANGE
+     * eingibt.
+     * @return gibt gültige Zahl
+     */
+    static int read_player_number () {
+        int player_quess;
+        do {
+            System.out.print("Enter your number between 0 and " + (COMPUTER_GUESS_RANGE - 1) + ": ");
+            player_quess = convertPlayerInputToNumber(scanner.next());
+        } while (player_quess == -1);
         return player_quess;
     }
 
