efd035008cacf838be5720fc07cc043bc6213edb
Daniel Herczeg <daniel.herczeg@tuwien.ac.at>
2025-02-13 13:20:50 +0100

    added game reatart system

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final int COMPUTER_GUESS_RANGE = 26;
+    private static final int COMPUTER_GUESS_RANGE = 6;
 
     static Scanner scanner = new Scanner(System.in);
     static Random rand = new Random();
@@ -51,24 +51,28 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int number_of_computer = rand.nextInt(COMPUTER_GUESS_RANGE);
-        int player_quess = read_player_number ();
+        do {
 
-        while (player_quess != number_of_computer) {
-            System.out.println ("false number");
+            int number_of_computer = rand.nextInt(COMPUTER_GUESS_RANGE);
+            int player_quess = read_player_number ();
 
-            if (player_quess > number_of_computer) {
-                System.out.println("number should be smaller");
-            } else {
-                System.out.println("number should be greater");
+            while (player_quess != number_of_computer) {
+                System.out.println ("false number");
+
+                if (player_quess > number_of_computer) {
+                    System.out.println("number should be smaller");
+                } else {
+                    System.out.println("number should be greater");
+                }
+
+                player_quess = read_player_number ();
+                numbers_of_quesses++;
             }
 
-            player_quess = read_player_number ();
-            numbers_of_quesses++;
-        }
-
-        System.out.println ("You win");
-        System.out.println ("it took you " + numbers_of_quesses + " quesses");
+            System.out.println ("You win");
+            System.out.println ("it took you " + numbers_of_quesses + " quesses");
+            System.out.println("wollen Sie weiter spielen? (y/n)");
+        } while (scanner.next().toLowerCase().charAt(0) == 'y');
     }
 
 }
